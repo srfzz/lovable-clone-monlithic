5fd36128f2ef7cfa8694f71aa9d6ed3f2cb7e1f3
Sarfaraj Ansari <imsarfaraaj7@gmail.com>
2026-04-04 13:54:14 +0530

    added usage log entity

--- src/main/java/com/strucify/projects/Lovable_clone/entity/UsageLog.java
+++ src/main/java/com/strucify/projects/Lovable_clone/entity/UsageLog.java
@@ -1,4 +1,15 @@
 package com.strucify.projects.Lovable_clone.entity;
 
+import java.time.Instant;
+
 public class UsageLog {
+    Long id;
+    User user;
+    Project project;
+    String action;
+    Integer tokenUsed;
+    Integer durationms;
+    String metaData;
+    Instant createdAt;
+
 }
